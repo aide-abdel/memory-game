ad9e945f3349cfd7c4768072c67242664ae25642
Coursera <aide.abdel@gmail.com>
2023-02-23 15:38:46 -0500

    flipping two matchin cards you win

--- app.js
+++ app.js
@@ -3,26 +3,32 @@ const cards = document.getElementsByClassName("card")
 
 let numberFlipped = 0;
 let cardsFlipped = [];
+let oldSrc = ""
 Array.from(cards).forEach(function(card) {
     card.addEventListener('click',()=>{
 
-        console.log(card.getElementsByClassName("back")[0].getElementsByClassName("backImg")[0].src);
+        let imgSrc = card.getElementsByClassName("back")[0].getElementsByClassName("backImg")[0].src;
+
 
         if(numberFlipped === 0){
             card.classList.toggle("flipCard");
             cardsFlipped[numberFlipped] = card;
+            oldSrc = imgSrc;
             numberFlipped++;
             console.log("First click");
         } else if(numberFlipped === 1){
             
             card.classList.toggle("flipCard");
+            if(oldSrc === imgSrc){
+                console.log("You Win!!!")
+            }
             setTimeout(function(){
                 card.classList.toggle("flipCard");
                 cardsFlipped[numberFlipped-1].classList.toggle("flipCard");
                 console.log("Second click");
                 
             numberFlipped++;
-            },2000);
+            },1000);
             
         } else{
             console.log("no more")
