4788ed75b09526121d0d288e4556b27deb2e3a0d
Coursera <aide.abdel@gmail.com>
2023-02-23 13:48:39 -0500

    check matching cards

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-// const cardOne = document.getElementById("cardOne");
-const card = document.querySelector(".card")
+const cards = document.getElementsByClassName("card")
 
-card.addEventListener('click',()=>{
-    // cardOne.style.backgroundColor = "green";
-    // cardOne.style.transform = "rotateY(180deg)";
-    card.classList.toggle("flipCard")
-})
+let numberFlipped = 0;
+let cardsFlipped = [];
+Array.from(cards).forEach(function(card) {
+    card.addEventListener('click',()=>{
+
+
+        if(numberFlipped === 0){
+            card.classList.toggle("flipCard");
+            cardsFlipped[numberFlipped] = card;
+            numberFlipped++;
+            console.log("First click");
+        } else if(numberFlipped === 1){
+            card.classList.toggle("flipCard");
+            setTimeout(function(){
+                card.classList.toggle("flipCard");
+                cardsFlipped[numberFlipped-1].classList.toggle("flipCard");
+                console.log("Second click");
+                
+            numberFlipped++;
+            },2000);
+            
+        } else{
+            console.log("no more")
+        }
+        
+    })
+  });
+
+//logic of the game
+
+function flipBack(cardOne,cardTwo){
+    cardOne.classList.toggle("flipCard");
+    cardTwo.classList.toggle("flipCard");
+}
+
+
