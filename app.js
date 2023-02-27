076106eec323e051cd8935523770fb59320f7f2c
aide-abdel <49523120+aide-abdel@users.noreply.github.com>
2023-02-27 14:11:37 -0500

    add shuffle function

--- app.js
+++ app.js
@@ -58,3 +58,20 @@ card.imgSrc = imagesSources[i];
 card.id = document.getElementById("card"+i);
 cards.push(card);
 }
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
